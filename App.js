455116555a1ad4d7790c6a1ae1881a50120c8cfd
Muhammad Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-07-27 09:50:20 +0500

    simple Todo made

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import Task from './components/Tasks';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+     setTaskItems([...taskItems, task]);
+     setTask(null);
+  }
+
+  const completeTask =(index) =>{
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
   return (
     <View style={styles.container}>
       {/* Todays tasks */}
@@ -13,9 +28,18 @@ export default function App() {
 
        <View style = {styles.items}>
         {/* This is the place where all the items are listed */}
-       <Task text = "Task 1"/>
-       <Task text = "Task 2"/>
-       <Task text = "Task 3"/>
+        {
+          taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity  key = {index} onPress={() => completeTask(index)}>
+                      <Task  text = {item}/>
+              </TouchableOpacity>
+            )
+            
+            
+          })
+        }
+       
 
 
        </View>
@@ -30,9 +54,11 @@ export default function App() {
     <TextInput
     style = {styles.input}
     placeholder = {'Write a task'}
+    onChangeText = {text => setTask(text)}
+    value = {task}
     />
      
-    <TouchableOpacity>
+    <TouchableOpacity onPress={() => handleAddTask()}>
       <View style = {styles.addWrapper}>
         <Text style = {styles.addText}>+</Text>
       </View>
@@ -69,11 +95,29 @@ wtiteTaskWrapper: {
   bottom: 60,
   width: '100%',
   flexDirection: 'row',
-  justifyContent: 'space-between',
+  justifyContent: 'space-around',
   alignItems: 'center',
 },
-input: {},
-addWrapper: {},
+input: {
+  paddingVertical: 15,
+  backgroundColor: '#FFF',
+  width: 250,
+  paddingHorizontal: 15,
+  borderRadius: 60,
+  borderWidth: 1,
+  borderColor: '#C0C0C0',
+},
+addWrapper: {
+  width:60,
+  height: 60,
+  backgroundColor : '#FFF',
+  borderRadius: 60,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderWidth: 1,
+  borderColor: '#C0C0C0',
+
+},
 addText: {}
 
   
